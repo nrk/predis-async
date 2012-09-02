d66418c0f60a9e7a833d873ae004b3f2198a0023
Daniele Alessandri <suppakilla@gmail.com>
2012-09-02 15:23:15 +0200

    We don't use a logger client in the Pub/Sub example.

--- examples/PubSub.php
+++ examples/PubSub.php
@@ -9,9 +9,7 @@ $client = new Predis\Async\Client('tcp://127.0.0.1:6379', $loop = new EventLoop(
 $client->connect(function ($client) {
     echo "Connected to Redis, now listening for incoming messages...\n";
 
-    $logger = new Predis\Async\Client('tcp://127.0.0.1:6379', $client->getEventLoop());
-
-    $client->pubsub('nrk:channel', function ($event, $pubsub) use ($logger) {
+    $client->pubsub('nrk:channel', function ($event, $pubsub) {
         $message = "Received message `%s` from channel `%s` [type: %s].\n";
 
         $feedback = sprintf($message,
