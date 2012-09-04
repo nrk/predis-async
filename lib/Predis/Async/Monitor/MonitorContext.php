e187e7e8e7e849d934d7f52ce66082d67696bbdd
Daniele Alessandri <suppakilla@gmail.com>
2012-09-04 15:43:35 +0200

    Add missing heading to files.

--- lib/Predis/Async/Monitor/MonitorContext.php
+++ lib/Predis/Async/Monitor/MonitorContext.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Predis\Async package.
+ *
+ * (c) Daniele Alessandri <suppakilla@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Predis\Async\Monitor;
 
 use InvalidArgumentException;
