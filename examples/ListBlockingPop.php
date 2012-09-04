4bea6454d8d1a773e0d407132c447dd272b2cc5c
Daniele Alessandri <suppakilla@gmail.com>
2012-09-04 16:31:01 +0200

    Add an example of BLPOP.

---
+++ examples/ListBlockingPop.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Predis\Async package.
+ *
+ * (c) Daniele Alessandri <suppakilla@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'/../autoload.php';
+
+use React\EventLoop\StreamSelectLoop as EventLoop;
+
+$server = 'tcp://127.0.0.1:6379';
+$loop = new EventLoop();
+
+$consumer = new Predis\Async\Client($server, $loop);
+$producer = new Predis\Async\Client($server, $loop);
+
+$consumer->connect(function ($consumer) use ($producer) {
+    echo "Connected to Redis, will BLPOP for max 10 seconds on `nrk:queue` and produce an item in ~5 seconds.\n";
+
+    $start = microtime(true);
+
+    $consumer->blpop('nrk:queue', 10, function ($response) use ($consumer, $producer, $start) {
+        list($queue, $stop) = $response;
+
+        $seconds = round((float) $stop - $start, 3);
+        echo "Received item from `$queue` after $seconds seconds!\n";
+
+        $consumer->disconnect();
+        $producer->disconnect();
+    });
+
+    $consumer->getEventLoop()->addTimer(5, function () use ($producer) {
+        $producer->lpush('nrk:queue', $microtime = microtime(true), function () use ($microtime) {
+            echo "Just pushed $microtime to `nrk:queue`.\n";
+        });
+    });
+});
+
+$loop->run();
