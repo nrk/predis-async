8ec47c94ef62c097064d64c6fecf28e11cc4c4a6
Daniele Alessandri <suppakilla@gmail.com>
2014-10-26 10:57:29 +0100

    Fix builds for HHVM on Travis CI.

---
+++ travisci-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -ef -o pipefail
+
+if  [ -z "$TRAVIS_PHP_VERSION" ]; then
+    echo "This script is meant to be executed on Travis CI."
+    exit 1
+fi
+
+if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
+    git clone https://github.com/redis/hiredis.git \
+    && pushd hiredis \
+    && make \
+    && sudo make install \
+    && popd
+
+    git clone https://github.com/nrk/phpiredis.git \
+    && pushd phpiredis \
+    && phpize \
+    && ./configure --enable-phpiredis \
+    && make \
+    && make install \
+    && popd
+
+    phpenv config-add phpiredis.ini
+fi
+
+composer self-update
